c074a8b49bd0c64c3d5396515b38172190434d84
Jule Schatz <drschatz@illinois.edu>
2024-08-27 16:11:12 -0500

    adjusted driver file

--- src/driver_full.cc
+++ src/driver_full.cc
@@ -1,18 +1,19 @@
-#include <fstream>
-#include <iostream>
-#include <map>
-#include <ostream>
-#include <string>
-#include <vector>
+//ADD include statements needed here
 
 #include "word_guess_functions.hpp"
 
 using namespace std;
+
+// Write your driver for part 2 in this file. 
+
+// Create and use your own functions in this file. 
+// You do not want your main function over ~40 lines 
+// of code to help with readability and reusability. 
+
 
 // cout statements
 // cout << "Select a mode, neutral or evil : ";
 // cout << "Invalid. Please pick neutral, or evil : ";
 string SelectMode() {}
 
-// Write your driver for part 2 here
 int main() { string mode = SelectMode(); }
